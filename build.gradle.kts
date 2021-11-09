895bdfabe14ec8c9a57580e26680ba0ba4689d37
Robert Elliot <rob@lidalia.org.uk>
2021-11-09 17:00:07 +0000

    Format kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,75 +4,75 @@ import org.jmailen.gradle.kotlinter.KotlinterPlugin
 val javaVersion by extra(JavaLanguageVersion.of(17))
 
 plugins {
-    base
-    kotlin("jvm") version "1.6.0-RC2" apply false
-    id("org.jmailen.kotlinter") version "3.6.0"
-    id("com.github.ben-manes.versions") version "0.39.0"
+  base
+  kotlin("jvm") version "1.6.0-RC2" apply false
+  id("org.jmailen.kotlinter") version "3.6.0"
+  id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 allprojects {
-    group = "uk.org.lidalia.petstore"
-    version = "0.1.0"
+  group = "uk.org.lidalia.petstore"
+  version = "0.1.0"
 
-    repositories {
-        mavenCentral()
-    }
-    apply<KotlinterPlugin>()
+  repositories {
+    mavenCentral()
+  }
+  apply<KotlinterPlugin>()
 }
 
 subprojects {
 
-    pluginManager.withPlugin("kotlin") {
-        configure<JavaPluginExtension> {
-            toolchain {
-                languageVersion.set(javaVersion)
-            }
+  pluginManager.withPlugin("kotlin") {
+    configure<JavaPluginExtension> {
+      toolchain {
+        languageVersion.set(javaVersion)
+      }
 
-            consistentResolution {
-                useCompileClasspathVersions()
-            }
-        }
-
-        configurations.all {
-            resolutionStrategy {
-                failOnNonReproducibleResolution()
-            }
-        }
-
-        dependencies {
-
-            val testImplementation by configurations
-
-            testImplementation(libs.kotest.runnerJunit5)
-        }
-
-        tasks {
-
-            withType<KotlinCompile>().configureEach {
-                kotlinOptions.apply {
-                    jvmTarget = javaVersion.toString()
-//                    freeCompilerArgs = listOf("-Xinline-classes")
-                }
-            }
-
-            named<Test>("test") {
-                environment("BUILD_SYSTEM", "GRADLE")
-                useJUnitPlatform()
-//                jvmArgs(
-//                    "-Xshare:off",
-//                    "--illegal-access=deny"
-//                )
-            }
-        }
-
-        kotlinter {
-            reporters = arrayOf("checkstyle", "plain", "html")
-        }
+      consistentResolution {
+        useCompileClasspathVersions()
+      }
     }
+
+    configurations.all {
+      resolutionStrategy {
+        failOnNonReproducibleResolution()
+      }
+    }
+
+    dependencies {
+
+      val testImplementation by configurations
+
+      testImplementation(libs.kotest.runnerJunit5)
+    }
+
+    tasks {
+
+      withType<KotlinCompile>().configureEach {
+        kotlinOptions.apply {
+          jvmTarget = javaVersion.toString()
+//                    freeCompilerArgs = listOf("-Xinline-classes")
+        }
+      }
+
+      named<Test>("test") {
+        environment("BUILD_SYSTEM", "GRADLE")
+        useJUnitPlatform()
+//        jvmArgs(
+//          "-Xshare:off",
+//          "--illegal-access=deny"
+//        )
+      }
+    }
+
+    kotlinter {
+      reporters = arrayOf("checkstyle", "plain", "html")
+    }
+  }
 }
 
 tasks {
-    check {
-        dependsOn("installKotlinterPrePushHook")
-    }
+  check {
+    dependsOn("installKotlinterPrePushHook")
+  }
 }
